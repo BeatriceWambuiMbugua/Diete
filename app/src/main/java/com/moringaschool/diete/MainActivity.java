ccbe3deacac4016e61af7532b760e5e732446919
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2020-11-16 19:44:00 +0300

    working on the activity_main.xml and MainActivity.java

--- app/src/main/java/com/moringaschool/diete/MainActivity.java
+++ app/src/main/java/com/moringaschool/diete/MainActivity.java
@@ -3,12 +3,22 @@ package com.moringaschool.diete;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView (R.id.getRecipeButton) Button mGetRecipeButton;
+    @BindView(R.id.introductionEditText) EditText mIntroductionEditText;
+    @BindView (R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
