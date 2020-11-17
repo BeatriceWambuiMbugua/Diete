224e3dbeee7e12e6ff95fdb3505d55cecc73711c
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2020-11-17 12:46:48 +0300

    Activated the introduction text to test whether it is working.

--- app/src/main/java/com/moringaschool/diete/ui/RecipeActivity.java
+++ app/src/main/java/com/moringaschool/diete/ui/RecipeActivity.java
@@ -2,15 +2,29 @@ package com.moringaschool.diete.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.moringaschool.diete.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class RecipeActivity extends AppCompatActivity {
+    @BindView(R.id.introductionTextView) TextView mIntroductionTextView;
+   // @BindView(R.id.listView) ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String introduction = intent.getStringExtra("introduction");
+        mIntroductionTextView.setText("Here are the available Recipes for: " + introduction);
     }
 }
