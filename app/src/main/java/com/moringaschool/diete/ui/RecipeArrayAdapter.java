bc12c0be16643d1afa87156b8adff20331fa975c
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2020-11-17 14:54:13 +0300

    integrated the ArrayAdapter responsible for the ArrayList

--- app/src/main/java/com/moringaschool/diete/ui/RecipeArrayAdapter.java
+++ app/src/main/java/com/moringaschool/diete/ui/RecipeArrayAdapter.java
@@ -1,4 +1,32 @@
 package com.moringaschool.diete.ui;
 
-public class RecipeArrayAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class RecipeArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mRecipes;
+    private String[] mCookTime;
+
+    public RecipeArrayAdapter(Context mContext, int resource, String[] mRecipes, String[] mCookTime) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mRecipes = mRecipes;
+        this.mCookTime = mCookTime;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String recipe = mRecipes[position];
+        String cookTime = mCookTime[position];
+        return String.format("%s \nCook time is: %s", recipe, cookTime);
+    }
+
+    @Override
+    public int getCount() {
+        return mRecipes.length;
+    }
 }
