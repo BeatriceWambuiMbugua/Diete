654b1a6dc121c8481d7aa426a10dd2753b7f73cd
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2020-11-25 11:20:24 +0300

    integrated retrofit in the application to access the user's internet source

--- app/src/main/java/com/moringaschool/diete/Constants.java
+++ app/src/main/java/com/moringaschool/diete/Constants.java
@@ -1,4 +1,7 @@
 package com.moringaschool.diete;
 
+
 public class Constants {
+    public static final String SPOONACULAR_BASE_URL = "https://api.spoonacular.com/recipes";
+    public static final String SPOONACULAR_API_KEY = BuildConfig.SPOONACULAR_API_KEY;
 }
