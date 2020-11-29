85fdef331f09288aea0967a4ab55428cc63f895b
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2020-11-29 21:24:33 +0300

    created the RecyclerViewHomeAdapter and ViewPagerHeaderAdapter

--- app/src/main/java/com/moringaschool/diete/ui/HomePage.java
+++ app/src/main/java/com/moringaschool/diete/ui/HomePage.java
@@ -1,18 +1,30 @@
 package com.moringaschool.diete.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.moringaschool.diete.R;
+import com.moringaschool.diete.adapters.RecyclerViewHomeAdapter;
+import com.moringaschool.diete.adapters.ViewPagerHeaderAdapter;
 import com.moringaschool.diete.models.Categories;
 import com.moringaschool.diete.models.Meals;
 
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class HomePage extends AppCompatActivity implements HomeView {
+
+    @BindView(R.id.viewPageHeader) ViewPager viewPagerMeal;
+    @BindView(R.id.recyclerCategory)  RecyclerView recyclerViewCategory;
 
     RecipeActivity activity;
 
@@ -29,23 +41,34 @@ public class HomePage extends AppCompatActivity implements HomeView {
 
     @Override
     public void showLoading() {
+        findViewById(R.id.excitingMeal).setVisibility(View.VISIBLE);
+        findViewById(R.id.shimmerCategory).setVisibility(View.VISIBLE);
 
     }
 
     @Override
     public void hideLoading() {
-
+        findViewById(R.id.excitingMeal).setVisibility(View.GONE);
+        findViewById(R.id.shimmerCategory).setVisibility(View.GONE);
     }
 
     @Override
     public void setMeal(List<Meals.Meal> meal) {
-        for(Meals.Meal mealresult : meal){
-            Log.w("meal name: ", mealresult.getStrMeal());
+        ViewPagerHeaderAdapter headerAdapter = new ViewPagerHeaderAdapter(meal, this);
+        viewPagerMeal.setAdapter(headerAdapter);
+        viewPagerMeal.setPadding(20, 0, 150, 0);
+        headerAdapter.notifyDataSetChanged();
         }
-    }
 
     @Override
     public void setCategory(List<Categories.Category> category) {
+        RecyclerViewHomeAdapter homeAdapter = new RecyclerViewHomeAdapter(category, this);
+        recyclerViewCategory.setAdapter(homeAdapter);
+        Context context;
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false);
+        recyclerViewCategory.setLayoutManager(layoutManager);
+        recyclerViewCategory.setNestedScrollingEnabled(true);
+        homeAdapter.notifyDataSetChanged();
 
     }
 
