0c7fbd8ab6d8fd88908528e0eb03abd6c9c50b3c
BeatriceWambuiMbugua <wambuimbeatrice@gmail.com>
2020-12-01 09:12:37 +0300

    structured the activity_signup.xml

---
+++ app/src/main/java/com/moringaschool/diete/ui/Signup.java
@@ -0,0 +1,16 @@
+package com.moringaschool.diete.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.diete.R;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
